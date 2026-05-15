51fffa236b4b0516b57821692c6acff5b7f15d74
Ahmed Wael <133889737+shadow9-1-1@users.noreply.github.com>
2026-05-15 04:15:04 +0300

    Setup Prometheus Infrastructure #65

--- services/payment-service/src/app.js
+++ services/payment-service/src/app.js
@@ -4,12 +4,26 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./docs/swagger");
+const logger = require("./utils/logger");
+const { correlationMiddleware } = require("./utils/correlation");
 
 const healthRouter = require("./routes/health");
 const metricsRouter = require("./routes/metrics");
 const paymentRouter = require("./routes/payments");
 
 const app = express();
+
+app.use(correlationMiddleware);
+
+app.use((req, res, next) => {
+  logger.info("Incoming request", {
+    method: req.method,
+    url: req.url,
+    ip: req.ip,
+    userAgent: req.get("User-Agent"),
+  });
+  next();
+});
 
 app.use(helmet());
 app.use(cors());
@@ -36,6 +50,13 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;
+  logger.error("Error encountered", {
+    error: err.message,
+    stack: err.stack,
+    status,
+    url: req.url,
+    method: req.method,
+  });
   res.status(status).json({
     error: err.message || "Internal server error"
   });
